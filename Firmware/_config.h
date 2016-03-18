d26d0c991dadcff41b3742a73b85fd84ea47ec7a
Yves Medhard Tibe da Cunha Tibe-Bi <yvesmedhard@outlook.com>
2016-03-18 19:58:07 -0300

    Added the motors.cpp code
    Added motor 3 in config

--- Firmware/_config.h
+++ Firmware/_config.h
@@ -6,7 +6,18 @@
 
 //
 // PINS
-//
+//TODO: AJUST THE RIGHT PINS
+#define PIN_M1_EN							1
+#define PIN_M1_IN1							2
+#define PIN_M1_IN2							3
+
+#define PIN_M2_EN							4
+#define PIN_M2_IN1							5
+#define PIN_M2_IN2							6
+
+#define PIN_M3_EN							7
+#define PIN_M3_IN1							8
+#define PIN_M3_IN2							9
 
 //
 // Project
