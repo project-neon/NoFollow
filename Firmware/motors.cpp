9ac0758b106499abc8122e2977c1610214f4d375
Yves Medhard Tibe da Cunha Tibe-Bi <yvesmedhard@outlook.com>
2016-03-18 19:58:07 -0300

    Added the motors.cpp code
    Added motor 3 in config

---
+++ Firmware/motors.cpp
@@ -0,0 +1,75 @@
+#include <Arduino.h>
+
+#include "_config.h"
+#include "_types.h"
+
+#include "robot.h"
+#include "motors.h"
+
+float Motors::avgSpeed = 0;
+
+// Initialize pins
+void Motors::init(){
+	LOG("Motors::init\n");
+
+	pinMode(PIN_M1_EN, OUTPUT);
+	pinMode(PIN_M2_EN, OUTPUT);
+    pinMode(PIN_M3_EN, OUTPUT);
+	
+	pinMode(PIN_M1_IN1, OUTPUT);
+	pinMode(PIN_M2_IN1, OUTPUT);
+    pinMode(PIN_M3_IN1, OUTPUT);
+
+	pinMode(PIN_M1_IN2, OUTPUT);
+	pinMode(PIN_M2_IN2, OUTPUT);
+    pinMode(PIN_M3_IN2, OUTPUT);
+}
+
+// Set power of both DC motors
+void Motors::setPower(float m1, float m2){
+
+	Motors::avgSpeed = (m1 + m2) / 2.0;
+
+	// Limit Powers
+	m1 = min(max(m1, -100), 100);
+	m2 = min(max(m2, -100), 100);
+
+	// Map powers
+	int powerOutA = m1 * (MOTOR_ABS_MAX * (1/100.0));
+	int powerOutB = m2 * (MOTOR_ABS_MAX * (1/100.0));
+
+	// Set power
+	analogWrite(PIN_M1_EN, abs(powerOutA));
+	analogWrite(PIN_M2_EN, abs(powerOutB));
+
+	// Set Directions
+	digitalWrite(PIN_M1_IN1, powerOutA > 0 ? HIGH : LOW);
+	digitalWrite(PIN_M1_IN2, powerOutA > 0 ? LOW : HIGH);
+
+	digitalWrite(PIN_M2_IN1, powerOutB > 0 ? HIGH : LOW);
+	digitalWrite(PIN_M2_IN2, powerOutB > 0 ? LOW : HIGH);
+	
+}
+
+// Set angle of Servo motor
+void Motors::setAngle(float a){
+    //TODO: SETAR O ANGULO
+	
+}
+
+
+void Motors::stop(){
+	// Set power (0)
+	digitalWrite(PIN_M1_EN, LOW);
+	digitalWrite(PIN_M2_EN, LOW);
+    digitalWrite(PIN_M3_EN, LOW);
+
+	// Set both DIRS to 0
+	digitalWrite(PIN_M1_IN1, LOW);
+	digitalWrite(PIN_M2_IN1, LOW);
+    digitalWrite(PIN_M3_IN1, LOW);
+
+	digitalWrite(PIN_M1_IN2, LOW);
+	digitalWrite(PIN_M2_IN2, LOW);
+    digitalWrite(PIN_M3_IN2, LOW);
+}
--- Firmware/motors.h
+++ Firmware/motors.h
@@ -1,1 +1,30 @@
 //this class will have the motors configuration, monitoration and control
+#include "_config.h"
+#include "_types.h"
+
+#include "robot.h"
+
+#ifndef MOTORS_H
+#define MOTORS_H
+
+
+class Motors{
+public:
+	// Average of both motors
+	static float avgSpeed;
+    static float avgAngle;
+
+	// Initialize pins
+	static void init();
+
+	// Set power of both DC motors
+	static void setPower(float m1, float m2);
+
+    // Set angle of Servo motors
+	static void setAngle(float a);
+
+	// Set state as Iddle (Turn off motors)
+	static void stop();
+};
+
+#endif
