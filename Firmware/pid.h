0bbd8d22f4395e81ee462fcfac040b7b6258c0bf
Gustavo Silva de Paula <gustavo.silva7337@gmail.com>
2016-03-19 16:14:08 -0300

    Added the pid.cpp code
    That is the version v0.1 of the PID controller

--- Firmware/pid.h
+++ Firmware/pid.h
@@ -1,1 +1,29 @@
 //this class will contain all PID logic and processing
+#include "_config.h"
+#include "_types.h"
+
+#include "robot.h"
+
+#ifndef pid_H
+#define pid_H
+class PID{
+public:
+
+  float desired;     //< set point
+
+	float prevError;    //< previous error
+	float integ;        //< integral
+
+	float kp;           //< proportional gain
+	float ki;           //< integral gain
+	float kd;           //< derivative gain
+
+	float iLimit;       //< integral limit
+
+  PID(float kp, float ki, float kd, float _ilimit);
+  void setSetPoint(double _setPoint);
+  float update(float input, float dt);
+	void clear();
+};
+
+#endif
