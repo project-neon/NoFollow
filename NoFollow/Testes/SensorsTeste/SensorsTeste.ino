30038d6e30905b803a2b6f295b0196221721c8bc
Yves Medhard Tibe da Cunha Tibe-Bi <yvesmedhard@outlook.com>
2016-07-06 16:26:31 -0300

    Changed SensorTeste to detect position. Corrected declaration type in lineReader

--- NoFollow/Testes/SensorsTeste/SensorsTeste.ino
+++ NoFollow/Testes/SensorsTeste/SensorsTeste.ino
@@ -5,19 +5,52 @@
 #define PIN_MUX_AN 5
 #define PIN_MUX_ENABLE 16
 int values[16];
+int SENSOR_MIN;
+int SENSOR_MAX;
 static int S[] = {PIN_MUX_S0, PIN_MUX_S1, PIN_MUX_S2, PIN_MUX_S3};
   
 void readValues(){
-    for(uint8_t i = 0; i<16; i++){
-      for(int n = 0; i<4; i++){
-        digitalWrite(S[n], i&(0x1<<n));
+    for(uint8_t i = 8; i < 16; i++){
+      for(int n = 0; n < 4; n++){
+        digitalWrite(S[n], i & (0x1 << n));
       }
-      values [i] = analogRead(PIN_MUX_AN);
+      values[i] = analogRead(PIN_MUX_AN);
     }
   }
   int getValue(int i){
-    return values[i];
+  return values[i];
+}
+
+float getPosition(){
+  float weighted_sum = 0;
+  float sum = 0;
+  for(int i = 0; i < 9; i++){
+    float value = map(getValue(i), SENSOR_MIN, SENSOR_MAX, 0, 1);
+    weighted_sum = weighted_sum + value  * (i + 1);
+    sum = sum + value;
   }
+  float position = ((weighted_sum / sum) - 4.5) / 3.5;
+  return position;
+}
+
+void calibrateSensor(){
+  int min = 1024;
+  int max = -1;
+  for(int n = 0; n < 10; n++){
+    readValues();
+    for(int i = 0; i < 8; i++){
+      int value = getValue(i);
+      if(min > value){
+        min = value;
+      }
+      if(max < value){
+        max = value; 
+      }
+    }      
+  }
+  SENSOR_MIN = min;
+  SENSOR_MAX = max;    
+}
 
 void setup() {
   Serial.begin(9600);
@@ -27,19 +60,17 @@ void setup() {
   pinMode(PIN_MUX_S2, OUTPUT);
   pinMode(PIN_MUX_S3, OUTPUT);
   pinMode(PIN_MUX_AN, INPUT);
+  calibrateSensor();
+  
 }
 
 void loop() {
   digitalWrite(PIN_MUX_ENABLE, HIGH);
   readValues();
-  for(int i =0; i<16;i++){
-    Serial.print("\t");
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.print(getValue(i));
-  }
+  Serial.print("Position = ");
+  Serial.print(getPosition());
   Serial.println();
-  delay(300);
+  delay(100);
   
   
 }
