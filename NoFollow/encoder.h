7d7e2033b8e9903528b603b2819e4a58fbb9c9be
gustavosdepaula <sdepaula.gustavo@gmail.com>
2016-07-06 16:10:25 -0300

    Implemented the Encoder

---
+++ NoFollow/encoder.h
@@ -0,0 +1,22 @@
+#define encoder0PinA 2
+#define encoder0PinB 3
+
+#ifndef ENCODER_H
+#define ENCODER_H
+
+class Interface{
+public:
+  static void init();
+  static float getSpeed();
+  static void doEncoderA;
+  static void doEncoderB;
+  volatile unsigned int encoder0Pos = 0;
+  unsigned int tmp_Pos = 1;
+  float speed=0;
+  int turn=0;
+  boolean A_set;
+  boolean B_set;
+};
+
+#endif
+
