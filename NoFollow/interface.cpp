276656f0c22e381cd82c6060bd26d403e11a2798
Ivan Seidel <ivanseidel@gmail.com>
2016-07-06 14:40:05 -0300

    cleanup. implement line Reader and servos

--- NoFollow/interface.cpp
+++ NoFollow/interface.cpp
@@ -10,11 +10,33 @@
 #include "robot.h"
 #include "interface.h"
 
+//
+// Thread: Serial Checke
+//
+void threadSerial_run();
+Thread threadSerial(threadSerial_run, 5);
+
 // Adafruit_SSD1306 display();
-static int STICK[5] = {STICK_RELEASED, STICK_UP, STICK_LEFT, STICK_DOWN, STICK_RIGHT};
+static int STICK[] = {
+  STICK_RELEASED,
+  STICK_UP,
+  STICK_LEFT,
+  STICK_DOWN,
+  STICK_RIGHT,
+  STICK_CENTER
+};
+
 // Initialize pins
 void Interface::init(){
 	LOG("Interface::init\n");
+
+  // Initialize Joystick
+  pinMode(PIN_JOYSTICK, INPUT_PULLUP);
+
+  // Initialize Serial and add debug thread to system
+  Serial.begin(115200);
+  system.add(&threadSerial);
+
 	// display.begin(SSD1306_SWITCHCAPVCC, 0x3D);
 	// display.display();
 	//
@@ -25,17 +47,59 @@ void Interface::init(){
 	// display.drawPixel(10, 10, WHITE);
 	// display.display();
 }
-int Interface::readValue(int val){
-	int state =-1;
+
+int Interface::readButton(){
+	int state = 0;
 	int minError = 256;
-	val = val/4;
-	int lenght =sizeof(SITCK)/sizeof(STICK[0]
-	for(int i = 0; i < sizeof(SITCK)/sizeof(STICK[0]); i++){
-		int diff = val > STICK[i] ? val - STICK[i] : STICK[i] - val;
+  int lenght = sizeof(STICK) / sizeof(STICK[0]);
+	int val = analogRead(PIN_JOYSTICK) / 4;
+
+	for(int i = 0; i < sizeof(STICK) / sizeof(STICK[0]); i++){
+		int diff = abs(val - STICK[i]);
+
 		if(diff < minError){
 			minError = diff;
 			state = i;
 		}
-	return STICK[state];
 	}
+
+  return STICK[state];
+}
+
+
+// ====================================
+//   HANDLES USB SERIAL COMMUNICATION
+// ====================================
+void threadSerial_run(){
+  if(!Serial.available())
+    return;
+
+  char got = Serial.read();
+
+  LOG("got:"); LOG(got); LOG("\n");
+
+  // if(got == '1')
+  //   Motors::setPower(50, 0);
+  //
+  // else if(got == '2')
+  //   Motors::setPower(-50, 0);
+  //
+  // else if(got == '3')
+  //   Motors::setPower(0, 50);
+  //
+  // else if(got == '4')
+  //   Motors::setPower(0, -50);
+
+  // else if(got == '0')
+  //   Motors::stop();
+
+  if(got == 'b'){
+    LOG("bat: "); LOG(Robot::vbat); LOG("v\n");
+
+  }else if(got == 'h'){
+    // LOG("\n---- help ----\n");
+    // LOG("0: Stop motors\n");
+    // LOG("b: Get bat. voltage\n");
+    // LOG("i: robot ID\n");
+  }
 }
